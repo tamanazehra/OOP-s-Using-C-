907c3474d1d206ec7b87e8e7adc1524861017172
tamanazehra <zehratamana3@gmail.com>
2025-04-25 15:59:40 +0530

    Encapsulation and Inheritance

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,30 +1,70 @@
-﻿public class student
+﻿public class Grade
 {
-    private int Marks;
+    protected int Marks;
 
-    public void setGrade(int m)
+    public void setMarks(int m)
     {
-        if(m <= 100 && m >= 0)
+        if (m <= 100 && m >= 0)
         {
             Marks = m;
-        } 
+        }
         else
         {
             Console.WriteLine("Invalid Marks");
         }
     }
-    public int getGrade()
+    public int getMarks()
     {
         return Marks;
     }
+    public void displayGrade()
+    {
+        if (Marks >= 90)
+        {
+            Console.WriteLine("Grade A");
+        }
+        else if (Marks >= 80)
+        {
+            Console.WriteLine("Grade B");
+        }
+        else if (Marks >= 70)
+        {
+            Console.WriteLine("Grade C");
+        }
+        else if (Marks >= 60)
+        {
+            Console.WriteLine("Grade D");
+        }
+        else
+        {
+            Console.WriteLine("Grade F");
+        }
+    }
 }
 
-public class Progarm
+public class Student : Grade
+{
+    public string Name;
+    public void setName(string n)
+    {
+        Name = n;
+    }
+    public string getName()
+    {
+        return Name;
+    }
+
+}
+
+public class Program
 {
     static void Main(string[] args)
     {
-        student s = new student();
-        s.setGrade(10);
-        Console.WriteLine("Student grade is: " + s.getGrade());
+        Student s = new Student();
+        Grade g = new Grade();
+        s.setName("John");
+        s.setMarks(100);
+        Console.WriteLine( s.Name +" score is: " + s.getMarks());
+        s.displayGrade();
     }
 }
