ffe39fc876b35e0021a697286f25c0e62dddb0b6
tamanazehra <zehratamana3@gmail.com>
2025-04-25 12:27:22 +0530

    Objects and Classes

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Dog
+{
+    public string Name;
+    public void bark()
+    {
+        Console.WriteLine(Name + " says Woof!");
+        Console.ReadLine();
+    }
+}
+
+public class Car
+{
+    public string Model;
+    public void honk()
+    {
+        Console.WriteLine(Model + " says Beep!");
+        Console.ReadLine();
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+        dog.Name = "Buddy";
+        dog.bark();
+
+        Car car = new Car();
+        car.Model = "Toyota";
+        car.honk();
+    }
+
+
+
+
+}
