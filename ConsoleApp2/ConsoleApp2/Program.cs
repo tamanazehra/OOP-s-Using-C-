20ca80bb8a939243d2c1497a14d64c61648693e5
tamanazehra <zehratamana3@gmail.com>
2025-04-25 15:08:05 +0530

    Encapsulation

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿public class student
+{
+    private int Marks;
+
+    public void setGrade(int m)
+    {
+        if(m <= 100 && m >= 0)
+        {
+            Marks = m;
+        } 
+        else
+        {
+            Console.WriteLine("Invalid Marks");
+        }
+    }
+    public int getGrade()
+    {
+        return Marks;
+    }
+}
+
+public class Progarm
+{
+    static void Main(string[] args)
+    {
+        student s = new student();
+        s.setGrade(10);
+        Console.WriteLine("Student grade is: " + s.getGrade());
+    }
+}
